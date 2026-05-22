b3b5f2f58dad81de95bd8335ee715b03308dd255
Taeguk <136004088+jonathangrp@users.noreply.github.com>
2026-05-22 10:28:03 -0400

    Update index.js
    Added otp.js script

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
+app.use('/otp.js', (req, res, next) => {
+  res.set('Cache-Control', 'no-store');
+  next();
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
